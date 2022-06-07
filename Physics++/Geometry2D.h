4cda19d682d1da94df3a9a4197cd358b4f689d31
Nicolas Daniele <nicolasdan553@gmail.com>
2022-06-07 19:57:47 -0300

    Defined Circle

--- Physics++/Geometry2D.h
+++ Physics++/Geometry2D.h
@@ -7,6 +7,27 @@
 
 typedef vec2 Point2D;
 
+typedef struct Line2D
+{
+	Point2D start;
+	Point2D end;
 
+	inline Line2D() {}
+	inline Line2D(const Point2D& s, const Point2D& e)
+		: start(s), end(e) {}
+} Line2D;
+
+typedef struct Circle
+{
+	Point2D position;
+	float radius;
+
+	inline Circle() : radius(1.0f) {}
+	inline Circle(const Point2D& p, float r)
+		: position(p), radius(r) {}
+} Circle;
+
+float Legth(const Line2D& line);
+float LengthSq(const Line2D& line);
 
 #endif
