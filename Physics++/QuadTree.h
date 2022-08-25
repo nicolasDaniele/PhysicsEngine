a8724755316546785b377d43ffa6857ac1d444b0
Nicolas Daniele <nicolasdaniele@ravegan.com>
2022-08-25 16:43:06 -0300

    Created QuadTree.h file

---
+++ Physics++/QuadTree.h
@@ -0,0 +1,48 @@
+#pragma once
+#ifndef _H_QUAD_TREEE
+#define _H_QUAD_TREEE
+#include "Geometry2D.h"
+#include <vector>
+
+using std::vector;
+
+struct QuadTreeData
+{
+	void* object; // Placeholder for GameObject class
+	Rectangle2D bounds;
+	bool flag;
+
+	inline QuadTreeData(void* o, const Rectangle2D& b) :
+		object(o), bounds(b), flag(false) { }
+};
+
+class QuadTreeNode
+{
+protected:
+	vector<QuadTreeNode> children;
+	vector<QuadTreeData*> contents;
+	int currentDepth;
+	static int maxDepth;
+	static int maxObjectsPerNode;
+	Rectangle2D nodeBounds;
+
+public:
+	inline QuadTreeNode(const Rectangle2D& bounds) :
+		nodeBounds(bounds), currentDepth(0) { }
+	bool IsLeaf();
+	int NumObjects();
+	void Insert(QuadTreeData& data);
+	void Remove(QuadTreeData& data);
+	void Update(QuadTreeData& data);
+	void Shake();
+	void Split();
+	void Reset();
+	vector<QuadTreeData*>Query(const Rectangle2D& area);
+};
+
+typedef QuadTreeNode QuadTree;
+
+
+
+
+#endif
